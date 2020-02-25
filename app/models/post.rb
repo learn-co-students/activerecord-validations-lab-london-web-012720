3adb7318ee7d9e8c926f7525e2314ec3f5c5ef29
musta7188 <musta7188@hotmail.it>
2020-02-25 13:53:37 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,42 @@
 class Post < ActiveRecord::Base
+  
+    validates :title, presence: true
+    validates :content, :length => { minimum: 250 }
+    validates :summary, :length => { maximum: 250 }
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    
+    validate :title_is_bait
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+      ]
+    
+    def title_is_bait
+        
+            if CLICKBAIT_PATTERNS.none? { |pat| pat.match title}
+              errors.add(:title, "must be clickbait")
+            end
+    end
+
+
 end
+
+
+
+#     t.string   "name"
+#     t.string   "phone_number"
+#     t.datetime "created_at",   null: false
+#     t.datetime "updated_at",   null: false
+#   end
+
+#   create_table "posts", force: :cascade do |t|
+#     t.string   "title"
+#     t.text     "content"
+#     t.text     "summary"
+#     t.string   "category"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
